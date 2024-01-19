bad84ec8468c1fdcd992a17a5d669430185ecb02
hanbiao <hanbiao4079@gmail.com>
2024-01-19 09:52:02 +0800

    添加题目

--- src/first_complete_index.rs
+++ src/first_complete_index.rs
@@ -1,69 +1,31 @@
 use std::collections::HashMap;
 
 /// 2661.找出完整访问过一遍的行或列对应的arr下标i
-
 pub fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
     let mut map_stack = HashMap::new();
     for x in mat.iter().enumerate() {
-        map_stack.insert(format!("x{}", x.0), x.1.clone());
-    }
-    for i in 0..mat[0].len() {
-        let mut stack = vec![];
-        for x in mat.iter().enumerate() {
-            let num = x.1.clone();
-            stack.push(num[i]);
+        for y in x.1.iter().enumerate() {
+            map_stack.insert(y.1, (x.0, y.0));
         }
-        map_stack.insert(format!("y{}", i), stack);
     }
-
-    let mut i = 0;
-    for num in arr.iter().enumerate() {
-        i = num.0;
-        let mut ls_full = false;
-        for stack in map_stack.clone() {
-            if stack.1.contains(num.1) {
-                match map_stack.get(&stack.0).as_mut() {
-                    Some(&mut x) => {
-                        if x.len() > 0 {
-                            let mut s = vec![];
-                            for &i in x {
-                                if i == *num.1 {
-                                    continue;
-                                } else {
-                                    s.push(i);
-                                }
-                            }
-                            if s.len() == 0 {
-                                ls_full = true;
-                                break;
-                            }
-                            map_stack.insert(stack.0, s);
-                        } else {
-                            ls_full = true;
-                            break;
-                        }
-                    }
-                    None => {
-                        ls_full = true;
-                        break;
-                    }
-                }
+    let mut row = vec![0; mat.len()];
+    let mut col = vec![0; mat[0].len()];
+    for (i, num) in arr.iter().enumerate() {
+        if let Some(&x) = map_stack.get(&num) {
+            row[x.0] += 1;
+            col[x.1] += 1;
+            if row[x.0] == mat[0].len() || col[x.1] == mat.len() {
+              return i as i32;
             }
         }
-        if ls_full {
-            break;
-        }
     }
-
-    println!("{:?}", map_stack);
-
-    i as i32
+    0
 }
 
 #[test]
 fn _test() {
-    let arr = vec![1, 3, 4, 2];
-    let mat = vec![vec![1, 4], vec![2, 3]];
+    let arr = vec![6, 2, 3, 1, 4, 5];
+    let mat = vec![vec![5, 1], vec![2, 4], vec![6, 3]];
     assert_eq!(first_complete_index(arr, mat), 2);
 }
 
