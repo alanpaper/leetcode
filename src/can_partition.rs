d27ca2c13bc3f03230624db90b8f87ebedd362ef
hanbiao <hanbiao4079@gmail.com>
2025-04-09 14:59:28 +0800

    打包

--- src/can_partition.rs
+++ src/can_partition.rs
@@ -1,6 +1,5 @@
 // 416. 分割等和子集
 // 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
-
 pub fn can_partition(nums: Vec<i32>) -> bool {
     let sum: i32 = nums.iter().sum();
     if sum % 2 != 0 {
